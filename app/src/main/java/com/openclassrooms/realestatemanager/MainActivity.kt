fd41a0dcc51ff239b2e925f0c8de3d7a500acc91
Damien Gironnet <gironnetd@yahoo.fr>
2020-11-30 20:13:03 +0100

    Add NavigationView to ToolBar - Testing GREEN
    - Test if home icon is displayed - succeeded
    - Test if real estate line item is displayed - succeeded
    - Test if add real estate line item is displayed - succeeded
    - Test if simulation line item is displayed - succeeded

--- app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.kt
@@ -3,17 +3,30 @@ package com.openclassrooms.realestatemanager
 import android.os.Bundle
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.openclassrooms.realestatemanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolBar)
+
+        navController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration.Builder(navController.graph)
+                .setOpenableLayout(binding.drawerLayout)
+                .build()
+        binding.toolBar.setupWithNavController(navController, appBarConfiguration)
+
         configureTextViewMain()
         configureTextViewQuantity()
     }
