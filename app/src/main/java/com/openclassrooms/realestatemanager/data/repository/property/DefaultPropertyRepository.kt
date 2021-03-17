307bb55934f67f281937e37f490be7a5affef139
Damien Gironnet <gironnetd@yahoo.fr>
2021-03-17 12:03:34 +0100

    Add PropertyRepository implementation - Test GREEN
    - Test when has internet and local storage is empty - succeeded
    - Test when has internet and local storage is not empty - succeeded
    - Test when has no internet and local storage is empty - succeeded
    - Test when has no internet and local storage is not empty - succeeded

--- app/src/main/java/com/openclassrooms/realestatemanager/data/repository/property/DefaultPropertyRepository.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/data/repository/property/DefaultPropertyRepository.kt
@@ -6,6 +6,9 @@ import com.openclassrooms.realestatemanager.di.property.browse.BrowseScope
 import com.openclassrooms.realestatemanager.models.Property
 import com.openclassrooms.realestatemanager.util.NetworkConnectionLiveData
 import io.reactivex.Observable
+import io.reactivex.ObservableTransformer
+import io.reactivex.Single
+import io.reactivex.subjects.ReplaySubject
 import javax.inject.Inject
 
 @BrowseScope
@@ -17,7 +20,57 @@ constructor(
         private val propertyLocalDataSource: PropertyLocalDataSource
 ) : PropertyRepository {
 
+    private var cachedProperties: MutableList<Property> =  mutableListOf()
+    private var isInternetSubject: ReplaySubject<Boolean> = ReplaySubject.create()
+
+    init {
+        networkConnectionLiveData.observeForever { isInternetAvailable ->
+            isInternetSubject.onNext(isInternetAvailable)
+        }
+    }
+
     override fun findAllProperties(): Observable<List<Property>> {
-        TODO("Not yet implemented")
+        if(networkConnectionLiveData.value != null && cachedProperties.isNotEmpty()) {
+            return  Observable.just(cachedProperties)
+        }
+        return isInternetSubject.compose(allProperties())
+    }
+
+    private fun allProperties() = ObservableTransformer<Boolean, List<Property>> { internetAvailable ->
+        internetAvailable.flatMap { isInternetAvailable ->
+            if (isInternetAvailable) {
+                Single.zip(findRemoteProperties(), findLocalProperties(),
+                        { remoteProperties, localProperties -> remoteProperties + localProperties })
+                        .toObservable()
+                        .flatMapIterable { properties -> properties }
+                        .distinct(Property::id)
+                        .toList()
+                        .flatMap { properties ->
+                            if(properties != cachedProperties) {
+                                cachedProperties.clear()
+                                cachedProperties.addAll(properties)
+                                propertyLocalDataSource.saveProperties(properties)
+                                        .andThen(Single.just(properties))
+                            } else {
+                                Single.just(cachedProperties)
+                            }
+                        }.toObservable()
+            } else findLocalProperties().toObservable()
+        }
+    }
+
+    private fun findRemoteProperties(): Single<List<Property>> {
+        return propertyRemoteDataSource.findAllProperties()
+    }
+
+    private fun findLocalProperties(): Single<List<Property>> {
+        if(cachedProperties.isEmpty()) {
+            return propertyLocalDataSource.findAllProperties()
+                    .doOnSuccess { localProperties -> cachedProperties.addAll(localProperties) }
+        }
+        if(networkConnectionLiveData.value!!) {
+            return Single.just(cachedProperties)
+        }
+        return Single.just(emptyList())
     }
 }
