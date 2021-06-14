d33ba1ccad278842c036f29ec81ce88fcd3af23d
Damien Gironnet <gironnetd@yahoo.fr>
2021-06-14 16:54:17 +0200

    Add PhotoLocalDataSourceTest.kt Tests - GREEN
    - Test if one photo saved with success - succeeded
    - Test if photos saved with success - succeeded
    - Test if one photo found by id with success - succeeded
    - Test if photos found by ids with success - succeeded
    - Test if all photos found with success - succeeded
    - Test if one photo deleted with success - succeeded
    - Test if photos deleted by ids with success - succeeded
    - Test if photos deleted with success - succeeded
    - Test if all photos deleted with success - succeeded
    - Test if one photo updated with success - succeeded
    - Test if photos updated with success - succeeded

--- app/src/main/java/com/openclassrooms/realestatemanager/data/local/source/PhotoLocalDataSource.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/data/local/source/PhotoLocalDataSource.kt
@@ -1,58 +1,79 @@
 package com.openclassrooms.realestatemanager.data.local.source
 
 import com.openclassrooms.realestatemanager.data.local.dao.PhotoDao
+import com.openclassrooms.realestatemanager.data.local.provider.toList
 import com.openclassrooms.realestatemanager.data.source.PhotoDataSource
 import com.openclassrooms.realestatemanager.models.Photo
+import com.openclassrooms.realestatemanager.util.schedulers.SchedulerProvider
 import io.reactivex.Completable
 import io.reactivex.Single
 
 class PhotoLocalDataSource constructor(private val photoDao: PhotoDao): PhotoDataSource {
 
     override fun count(): Single<Int> {
-        TODO("Not yet implemented")
+        return Single.fromCallable { photoDao.count() }
+            .subscribeOn(SchedulerProvider.io())
     }
 
     override fun savePhoto(photo: Photo): Completable {
-        TODO("Not yet implemented")
+        return Completable.fromAction {
+            photoDao.savePhoto(photo = photo)
+        }.subscribeOn(SchedulerProvider.io())
     }
 
     override fun savePhotos(photos: List<Photo>): Completable {
-        TODO("Not yet implemented")
+        return Completable.fromAction {
+            photoDao.savePhotos(*photos.toTypedArray())
+        }.subscribeOn(SchedulerProvider.io())
     }
 
     override fun findPhotoById(id: String): Single<Photo> {
-        TODO("Not yet implemented")
+        return Single.fromCallable {
+            photoDao.findPhotoById(id).toList { Photo(it) }.single()
+        }.subscribeOn(SchedulerProvider.io())
     }
 
     override fun findPhotosByIds(ids: List<String>): Single<List<Photo>> {
-        TODO("Not yet implemented")
+        return Single.fromCallable { photoDao.findPhotosByIds(ids) }.subscribeOn(SchedulerProvider.io()).flatMap {
+            Single.just(it)
+        }
     }
 
     override fun findAllPhotos(): Single<List<Photo>> {
-        TODO("Not yet implemented")
+        return Single.fromCallable {
+            photoDao.findAllPhotos().toList { Photo(it) }
+        }.subscribeOn(SchedulerProvider.io()).flatMap {
+            Single.just(it)
+        }
     }
 
     override fun updatePhoto(photo: Photo): Completable {
-        TODO("Not yet implemented")
+        return Completable.fromAction { photoDao.updatePhoto(photo) }
+            .subscribeOn(SchedulerProvider.io())
     }
 
     override fun updatePhotos(photos: List<Photo>): Completable {
-        TODO("Not yet implemented")
+        return Completable.fromAction { photoDao.updatePhotos(*photos.toTypedArray()) }
+            .subscribeOn(SchedulerProvider.io())
     }
 
     override fun deletePhotosByIds(ids: List<String>): Completable {
-        TODO("Not yet implemented")
+        return Completable.fromAction { photoDao.deletePhotosByIds(ids) }
+            .subscribeOn(SchedulerProvider.io())
     }
 
     override fun deletePhotos(photos: List<Photo>): Completable {
-        TODO("Not yet implemented")
+        return Completable.fromAction { photoDao.deletePhotos(*photos.toTypedArray()) }
+            .subscribeOn(SchedulerProvider.io())
     }
 
     override fun deleteAllPhotos(): Completable {
-        TODO("Not yet implemented")
+        return Completable.fromAction { photoDao.deleteAllPhotos() }
+            .subscribeOn(SchedulerProvider.io())
     }
 
     override fun deleteById(id: String): Completable {
-        TODO("Not yet implemented")
+        return Completable.fromAction { photoDao.deleteById(id) }
+            .subscribeOn(SchedulerProvider.io())
     }
 }
