6b9783ef36b3fddcafaaa0804320ca8af6b4bba6
Damien Gironnet <gironnetd@yahoo.fr>
2021-06-22 17:48:18 +0200

    Add PhotoLocalStorageSourceTest.kt Tests - GREEN
    - Test if one photo saved with success - succeeded
    - Test if photos saved with success - succeeded
    - Test if one photo found by id with success - succeeded
    - Test if photos found by ids with success - succeeded
    - Test if all photos found with success - succeeded
    - Test if one photo deleted with success - succeeded
    - Test if photos deleted by ids with success - succeeded
    - Test if photos deleted with success - succeeded
    - Test if all photos deleted with success - succeeded
    - Test if one photo updated with success - succeeded
    - Test if photos updated with success - succeeded

--- app/src/main/java/com/openclassrooms/realestatemanager/data/local/storage/PhotoLocalStorageSource.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/data/local/storage/PhotoLocalStorageSource.kt
@@ -1,61 +1,250 @@
 package com.openclassrooms.realestatemanager.data.local.storage
 
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import com.openclassrooms.realestatemanager.models.Photo
+import com.openclassrooms.realestatemanager.models.storageLocalDatabase
+import com.openclassrooms.realestatemanager.util.Constants
 import io.reactivex.Completable
+import io.reactivex.Observable
 import io.reactivex.Single
 import java.io.File
+import java.io.FileOutputStream
 
 class PhotoLocalStorageSource constructor(private val cacheDir: File) {
-    fun count(): Single<Int> {
-        TODO("Not yet implemented")
-    }
+
+    fun count(): Single<Int> { return findAllPhotos().map { photos -> photos.size } }
 
     fun count(propertyId: String): Single<Int> {
-        TODO("Not yet implemented")
+        return findPhotosByPropertyId(propertyId).map { photos -> photos.size }
     }
 
     fun savePhoto(photo: Photo): Completable {
-        TODO("Not yet implemented")
+        return Completable.create { emitter ->
+            photo.bitmap?.let { bitmap ->
+                val outputStream = FileOutputStream(
+                    File(photo.storageLocalDatabase(cacheDir, true)), true)
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+                outputStream.close()
+                emitter.onComplete()
+            } ?: emitter.onError(NullPointerException("bitmap for photo ${photo.id} is null"))
+        }
     }
 
     fun savePhotos(photos: List<Photo>): Completable {
-        TODO("Not yet implemented")
+        return Observable.fromIterable(photos).flatMapCompletable { photo ->
+            photo.bitmap?.let { savePhoto(photo) } ?: Completable.complete()
+        }
     }
 
     fun findPhotoById(id: String): Single<Bitmap> {
-        TODO("Not yet implemented")
+        return Single.create { emitter ->
+            val bitmaps: MutableList<Bitmap> = mutableListOf()
+            val propertiesDir = File(cacheDir.absolutePath, Constants.PROPERTIES_COLLECTION)
+            propertiesDir.listFiles()?.let { propertiesDirListFiles ->
+                propertiesDirListFiles.forEach { propertyDir ->
+                    val photosDir = File(propertyDir, Constants.PHOTOS_COLLECTION)
+                    photosDir.listFiles()?.let { photosDirListFiles ->
+                        photosDirListFiles.forEach { photoDir ->
+                            if(photoDir.name.contains(id)) {
+                                photoDir.listFiles()?.let { photoFile ->
+                                    val bitmap = BitmapFactory.decodeFile(photoFile[0].toString())
+                                    emitter.onSuccess(bitmap)
+                                    //bitmaps.add(bitmap)
+                                }
+                            }
+                        }
+                    } ?: emitter.onError(NullPointerException("Photos cacheDir for Property: ${propertyDir.name} is null"))
+                }
+                //emitter.onSuccess(bitmaps)
+            } ?: emitter.onError(NullPointerException("Properties cacheDir is null"))
+        }
     }
 
     fun findPhotosByIds(ids: List<String>): Single<List<Bitmap>> {
-        TODO("Not yet implemented")
+        return Single.create { emitter ->
+            val bitmaps: MutableList<Bitmap> = mutableListOf()
+            val propertiesDir = File(cacheDir.absolutePath, Constants.PROPERTIES_COLLECTION)
+            propertiesDir.listFiles()?.let { propertiesDirListFiles ->
+                propertiesDirListFiles.forEach { propertyDir ->
+                    val photosDir = File(propertyDir, Constants.PHOTOS_COLLECTION)
+                    photosDir.listFiles()?.let { photosDirListFiles ->
+                        photosDirListFiles.forEach { photoDir ->
+                            if(ids.any { it in photoDir.name }) {
+                                photoDir.listFiles()?.let { photoFile ->
+                                    val bitmap = BitmapFactory.decodeFile(photoFile[0].toString())
+                                    bitmaps.add(bitmap)
+                                }
+                            }
+                        }
+                    } ?: emitter.onError(NullPointerException("Photos cacheDir for Property: ${propertyDir.name} is null"))
+                }
+                emitter.onSuccess(bitmaps)
+            } ?: emitter.onError(NullPointerException("Properties cacheDir is null"))
+        }
     }
 
     fun findAllPhotos(): Single<List<Bitmap>> {
-        TODO("Not yet implemented")
+        return Single.create { emitter ->
+            val bitmaps: MutableList<Bitmap> = mutableListOf()
+            val propertiesDir = File(cacheDir.absolutePath, Constants.PROPERTIES_COLLECTION)
+            propertiesDir.listFiles()?.let { propertiesDirListFiles ->
+                propertiesDirListFiles.forEach { propertyDir ->
+                    val photosDir = File(propertyDir, Constants.PHOTOS_COLLECTION)
+                    photosDir.listFiles()?.let { photosDirListFiles ->
+                        photosDirListFiles.forEach { photoDir ->
+                            photoDir.listFiles()?.let { photoFile ->
+                                val bitmap = BitmapFactory.decodeFile(photoFile[0].toString())
+                                bitmaps.add(bitmap)
+
+//                                if(propertyDir.name == propertiesDirListFiles.last().name &&
+//                                    photoDir.name == photosDirListFiles.last().name  ) {
+//                                    emitter.onSuccess(bitmaps)
+//                                }
+                            }
+                        }
+                    } ?: emitter.onError(NullPointerException("Photos cacheDir for Property: ${propertyDir.name} is null"))
+                }
+                emitter.onSuccess(bitmaps)
+            } ?: emitter.onSuccess(emptyList())
+        }
+    }
+
+    fun findPhotosByPropertyId(propertyId: String): Single<List<Bitmap>> {
+        return Single.create { emitter ->
+            val bitmaps: MutableList<Bitmap> = mutableListOf()
+            val propertiesDir = File(cacheDir.absolutePath, Constants.PROPERTIES_COLLECTION)
+            propertiesDir.listFiles()?.let { propertiesDirListFiles ->
+                propertiesDirListFiles.forEach { propertyDir ->
+                    if(propertyDir.name == propertyId) {
+                        val photosDir = File(propertyDir, Constants.PHOTOS_COLLECTION)
+                        photosDir.listFiles()?.let { photosDirListFiles ->
+                            photosDirListFiles.forEach { photoDir ->
+                                photoDir.listFiles()?.let { photoFile ->
+                                    val bitmap = BitmapFactory.decodeFile(photoFile[0].toString())
+                                    bitmaps.add(bitmap)
+                                }
+                            }
+                            emitter.onSuccess(bitmaps)
+                        } ?: emitter.onError(NullPointerException("Photos cacheDir for Property: ${propertyDir.name} is null"))
+                    }
+                }
+            } ?: emitter.onError(NullPointerException("Properties cacheDir is null"))
+        }
     }
 
     fun updatePhoto(photo: Photo): Completable {
-        TODO("Not yet implemented")
+        return Completable.create { emitter ->
+            photo.bitmap?.let { bitmap ->
+                val outputStream = FileOutputStream(
+                    File(photo.storageLocalDatabase(cacheDir, true)), true)
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+                outputStream.close()
+                emitter.onComplete()
+            } ?: emitter.onError(NullPointerException("bitmap for photo ${photo.id} is null"))
+        }
     }
 
     fun updatePhotos(photos: List<Photo>): Completable {
-        TODO("Not yet implemented")
+        return Observable.fromIterable(photos).flatMapCompletable { photo ->
+            photo.bitmap?.let { updatePhoto(photo) } ?: Completable.complete()
+        }
     }
 
     fun deletePhotosByIds(ids: List<String>): Completable {
-        TODO("Not yet implemented")
+        return Completable.create { emitter ->
+            val propertiesDir = File(cacheDir.absolutePath, Constants.PROPERTIES_COLLECTION)
+            propertiesDir.listFiles()?.let { propertiesDirListFiles ->
+                propertiesDirListFiles.forEach { propertyDir ->
+                    val photosDir = File(propertyDir, Constants.PHOTOS_COLLECTION)
+                    photosDir.listFiles()?.let { photosDirListFiles ->
+                        photosDirListFiles.forEach { photoDir ->
+                            if(ids.any { it in photoDir.name }) {
+                                photoDir.listFiles()?.let { photoDirListFiles ->
+                                    photoDirListFiles.forEach { photoFile ->
+                                        photoFile.delete()
+                                    }
+                                    photoDir.delete()
+                                }
+                                emitter.onComplete()
+                            }
+                        }
+                    } ?: emitter.onError(NullPointerException("Photos cacheDir for Property: ${propertyDir.name} is null"))
+                }
+            } ?: emitter.onError(NullPointerException("Properties cacheDir is null"))
+        }
     }
 
     fun deletePhotos(photos: List<Photo>): Completable {
-        TODO("Not yet implemented")
+        return Completable.create { emitter ->
+            val propertiesDir = File(cacheDir.absolutePath, Constants.PROPERTIES_COLLECTION)
+            propertiesDir.listFiles()?.let { propertiesDirListFiles ->
+                propertiesDirListFiles.forEach { propertyDir ->
+                    val photosDir = File(propertyDir, Constants.PHOTOS_COLLECTION)
+                    photosDir.listFiles()?.let { photosDirListFiles ->
+                        photosDirListFiles.forEach { photoDir ->
+                            if(photos.map { it.id }.any { it in photoDir.name }) {
+                                photoDir.listFiles()?.let { photoDirListFiles ->
+                                    photoDirListFiles.forEach { photoFile ->
+                                        photoFile.delete()
+                                    }
+                                    photoDir.delete()
+                                }
+                            }
+                        }
+                    } ?: emitter.onError(NullPointerException("Photos cacheDir for Property: ${propertyDir.name} is null"))
+                }
+                emitter.onComplete()
+            } ?: emitter.onError(NullPointerException("Properties cacheDir is null"))
+        }
     }
 
     fun deleteAllPhotos(): Completable {
-        TODO("Not yet implemented")
+        return Completable.create { emitter ->
+            val propertiesDir = File(cacheDir.absolutePath, Constants.PROPERTIES_COLLECTION)
+            propertiesDir.listFiles()?.let { propertiesDirListFiles ->
+                propertiesDirListFiles.forEach { propertyDir ->
+                    val photosDir = File(propertyDir, Constants.PHOTOS_COLLECTION)
+                    photosDir.listFiles()?.let { photosDirListFiles ->
+                        photosDirListFiles.forEach { photoDir ->
+                            photoDir.listFiles()?.let { photoDirListFiles ->
+                                photoDirListFiles.forEach { photoFile ->
+                                    photoFile.delete()
+                                }
+                                photoDir.delete()
+                            }
+                        }
+                        photosDir.delete()
+                        propertyDir.delete()
+                    }
+                }
+                propertiesDir.delete()
+                emitter.onComplete()
+            } ?: emitter.onError(NullPointerException("Properties cacheDir is null"))
+        }
     }
 
     fun deletePhotoById(id: String): Completable {
-        TODO("Not yet implemented")
+        return Completable.create { emitter ->
+            val propertiesDir = File(cacheDir.absolutePath, Constants.PROPERTIES_COLLECTION)
+            propertiesDir.listFiles()?.let { propertiesDirListFiles ->
+                propertiesDirListFiles.forEach { propertyDir ->
+                    val photosDir = File(propertyDir, Constants.PHOTOS_COLLECTION)
+                    photosDir.listFiles()?.let { photosDirListFiles ->
+                        photosDirListFiles.forEach { photoDir ->
+                            if(photoDir.name.contains(id)) {
+                                photoDir.listFiles()?.let { photoDirListFiles ->
+                                    photoDirListFiles.forEach { photoFile ->
+                                        photoFile.delete()
+                                    }
+                                    photoDir.delete()
+                                }
+                                emitter.onComplete()
+                            }
+                        }
+                    } ?: emitter.onError(NullPointerException("Photos cacheDir for Property: ${propertyDir.name} is null"))
+                }
+            } ?: emitter.onError(NullPointerException("Properties cacheDir is null"))
+        }
     }
 }
