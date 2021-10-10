b16d18d499d4cefe502883499a0ba8a2c33debaa
Damien Gironnet <gironnetd@yahoo.fr>
2021-10-10 13:46:31 +0200

    Add Detekt - Static code analysis for Kotlin
    - Add detekt configuration to project build.gradle.kts
    - Add config/baseline.xml file
    - Add config/detekt.yml file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import io.gitlab.arturbosch.detekt.Detekt
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
@@ -16,6 +18,45 @@ buildscript {
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
+}
+
+plugins {
+    id("io.gitlab.arturbosch.detekt").version("1.18.1")
+}
+
+detekt {
+    toolVersion = "1.18.1"
+    buildUponDefaultConfig = true // preconfigure defaults
+    allRules = true // activate all available (even unstable) rules.
+    config = files("$projectDir/config/detekt.yml") // point to your custom config defining rules to run, overwriting default behavior
+    baseline = file("$projectDir/config/baseline.xml") // a way of suppressing issues before introducing detekt
+
+    source = files("$projectDir/app/src/main/kotlin")
+
+    reports {
+        xml {
+            xml.enabled = true
+            xml.destination = file("$projectDir/build/detekt/report.xml")
+        }
+        html {
+            html.enabled = true
+            html.destination = file("$projectDir/build/detekt/report.html")
+        }
+        txt {
+            txt.enabled = true
+            txt.destination = file("$projectDir/build/detekt/report.txt")
+        }
+    }
+}
+
+// Kotlin DSL
+tasks.withType<Detekt>().configureEach {
+    // Target version of the generated JVM bytecode. It is used for type resolution.
+    jvmTarget = "1.8"
+}
+
+dependencies {
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.18.1")
 }
 
 allprojects {
